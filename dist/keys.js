9e91d928bcdb57261d2431018ddcd0f0c2d0bf9e
Rachel Ralston <rachelralston@gmail.com>
2016-08-11 13:47:24 -0700

    addressing PR comments

--- dist/keys.js
+++ dist/keys.js
@@ -5,13 +5,11 @@ Object.defineProperty(exports, "__esModule", {
 });
 var keys = function keys(object) {
   var values = [];
-  // for( var key in object ) {
-  //   return Object.keys(object)
-  // }
-  // return values
+
   for (var key in object) {
     values.push(key);
   }
   return values;
 };
+
 exports.keys = keys;
--- src/keys.js
+++ src/keys.js
@@ -1,13 +1,10 @@
 const keys = object => {
   let values = []
-  // for( var key in object ) {
-  //   return Object.keys(object)
-  // }
-  // return values
+
   for( var key in object ) {
       values.push( key )
   }
   return values
-
 }
+
 export {keys}
