251b6f1b848780c732d8919add267d1143fd2a11
Rachel Ralston <rachelralston@gmail.com>
2016-08-11 10:31:04 -0700

    finished uniqueID

--- dist/uniqueID.js
+++ dist/uniqueID.js
@@ -1,10 +1,15 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+var id = -1;
+
 var uniqueID = function uniqueID() {
-  return 1 + 1;
+  var prefix = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+
+  id++;
+  return prefix === '' ? id + '' : prefix + '-' + id;
 };
 
 exports.uniqueID = uniqueID;
--- src/uniqueID.js
+++ src/uniqueID.js
@@ -1,3 +1,8 @@
-const uniqueID = () => 1+1
+let id = -1
+
+const uniqueID = ( prefix='' ) => {
+  id++
+  return prefix === '' ? id + '' : `${prefix}-${id}`
+}
 
 export { uniqueID }
